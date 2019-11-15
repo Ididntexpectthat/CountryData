ab331b754f831472ba737de09f613246be70cd9d
1069267922@qq.com <1069267922@qq.com>
2019-11-15 15:55:05 +0800

    2019/11/15/3

--- src/main/java/com/ydl/interceptor/AuthenticationInterceptor.java
+++ src/main/java/com/ydl/interceptor/AuthenticationInterceptor.java
@@ -88,6 +88,7 @@ public class AuthenticationInterceptor implements HandlerInterceptor {
                 try {
                     if (!StringUtils.isEmpty(redistoken) && redistoken.equals(token)) {
 //                        String Newtoken = UUID.randomUUID().toString().replaceAll("-", "");
+
                         tokenService.redisSaveToken(username, redistoken, 60);
                         System.out.println("校验成功");
                         return true;
@@ -98,6 +99,7 @@ public class AuthenticationInterceptor implements HandlerInterceptor {
                 }
             }
         }
+
         return true;
     }
 
